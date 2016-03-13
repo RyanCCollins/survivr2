72e988cd1b097223f287cc6306d579017e704f12
Ryan Collins <rccproductions@mac.com>
2016-03-13 15:08:51 -0400

    Test: all jury tests pass

--- lib/jury.rb
+++ lib/jury.rb
@@ -1,14 +1,35 @@
 class Jury
-  attr_reader :members
+  attr_accessor :members
 
-  @@members = []
   def initialize
-    #code
+    @members = []
   end
-  def add_member member
-    @@members << member
+  def add_member name
+    @members << name
   end
   def members
-    @@members
+    @members
+  end
+  def cast_votes finalists
+    votes = Hash.new
+    finalists.each do |finalist|
+      votes[finalist] = 0
+    end
+
+    @members.each do |member|
+      vote = finalists.sample
+      puts "#{member} votes #{vote}"
+      votes[vote] += 1
+    end
+    votes
+  end
+  def report_votes votes
+    votes.each do |finalist, vote|
+      puts "#{finalist} #{vote}"
+    end
+  end
+  def announce_winner votes
+    winner = votes.max_by { |fin, vote| vote }.first
+    winner
   end
 end
--- lib/tribe.rb
+++ lib/tribe.rb
@@ -1,3 +1,23 @@
 class Tribe
-  #code
+  attr_reader :name, :members, :color
+
+  def initialize opts={}
+  	@name = opts[:name]
+  	@members = opts[:members]
+  	@color = opts[:color]
+    puts "#{@name} created."
+  end
+
+  def to_s
+    @name.send @color.to_sym
+  end
+
+  def tribal_council immune=nil
+  	@members.reject { |member| member == immune }.sample
+  end
+
+  def eliminate_player player
+    @members -= [player]
+  end
+
 end
